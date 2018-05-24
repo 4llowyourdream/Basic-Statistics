360ceb2492df0c4c9286481fa23580e3c252baa7
4llowyourdream <33521494+4llowyourdream@users.noreply.github.com>
2018-05-24 21:50:20 +0300

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -3,7 +3,7 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 install_version("ggplot2", "2.2.1")
-install_version("ggplot2", "1.8.4")
+install_version("psych", "1.8.4")
 # install_version("dplyr", "0.7.5")
 # install_version("tidyr", "0.8.1")
 # install_version("purrr", "0.2.4")
